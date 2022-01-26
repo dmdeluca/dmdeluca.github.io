aa36847ef88958036fd7eea21d9a4df875e507d9
David DeLuca <dvdmdlc@gmail.com>
2022-01-26 15:13:38 -0500

    small update

---
+++ docs/populate-footer.js
@@ -0,0 +1,6 @@
+function populateFooter() {
+  const footer = document.createElement('footer');
+  footer.innerHTML = '<div><a href="https://dmdeluca.github.io">Home</a></div>';
+  document.appendChild(footer);
+  console.log('test');
+};
